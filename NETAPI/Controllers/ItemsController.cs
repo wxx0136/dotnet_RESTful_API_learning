fb9441cf92ce173537c1a59fe5bfaf8747a82211
wxx <wxx0136@gmail.com>
2021-02-12 22:39:08 -0600

    Add Entity, Repository and Controller

---
+++ NETAPI/Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NETAPI.Entities;
+using NETAPI.Repositories;
+
+namespace NETAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+}
---
+++ NETAPI/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NETAPI.Entities;
+
+namespace NETAPI.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> _items = new()
+        {
+            new Item {Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreateDate = DateTimeOffset.UtcNow},
+            new Item {Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreateDate = DateTimeOffset.UtcNow},
+            new Item {Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreateDate = DateTimeOffset.UtcNow}
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return _items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return _items.SingleOrDefault(item => item.Id == id);
+
+        }
+    }
+}
