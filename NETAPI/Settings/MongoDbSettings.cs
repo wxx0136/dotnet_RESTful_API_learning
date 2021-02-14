aa09a455789e7d3b4c1637621231a6f83c69c5b3
wxx <wxx0136@gmail.com>
2021-02-14 17:36:58 -0600

    Add Secrets and Health Checks

--- NETAPI/Settings/MongoDbSettings.cs
+++ NETAPI/Settings/MongoDbSettings.cs
@@ -9,7 +9,9 @@ namespace NETAPI.Settings
     {
         public string Host { get; set; }
         public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
 
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString => $"mongodb://{User}:{Password}@{Host}:{Port}";
     }
 }
--- NETAPI/Startup.cs
+++ NETAPI/Startup.cs
@@ -1,7 +1,14 @@
+using System;
+using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using MongoDB.Bson;
@@ -27,21 +34,23 @@ namespace NETAPI
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
-            services.AddSingleton<IMongoClient>(serviceProvider =>
-            {
-                var settings = Configuration.GetSection(nameof(MongoDbSettings))
-                    .Get<MongoDbSettings>();
-                return new MongoClient(settings.ConnectionString);
-            });
+            // ReSharper disable once UnusedParameter.Local
+            services.AddSingleton<IMongoClient>(serviceProvider => new MongoClient(mongoDbSettings.ConnectionString));
+
             //services.AddSingleton<IItemsRepository, InMemItemsRepository>();
             services.AddSingleton<IItemsRepository, MongoDbItemsRepositories>();
 
-            services.AddControllers(options =>
-            {
-                options.SuppressAsyncSuffixInActionNames = false;
-            });
+            services.AddControllers(options => { options.SuppressAsyncSuffixInActionNames = false; });
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "NETAPI", Version = "v1"}); });
+
+            services.AddHealthChecks().AddMongoDb(
+                mongoDbSettings.ConnectionString,
+                name: "mongodb",
+                timeout: TimeSpan.FromSeconds(3),
+                tags: new[] {"ready"}
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,9 +69,37 @@ namespace NETAPI
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+                {
+                    Predicate = check => check.Tags.Contains("ready"),
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new
+                            {
+                                status = report.Status.ToString(),
+                                checks = report.Entries.Select(entry => new
+                                {
+                                    name = entry.Key,
+                                    status = entry.Value.Status.ToString(),
+                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                    duration = entry.Value.Duration.ToString()
+                                })
+                            }
+                        );
 
-            //GIT push test
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+                {
+                    Predicate = _ => false
+                });
+            });
         }
     }
 }
